a176403bfcb2bed5cc6a669ad6ad4012df949d6b
jonathanlombardo <jonathanlombardo90@gmail.com>
2024-02-19 13:43:11 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script>
+// import MyComp from './components/MyComp.vue';
+// import {store} from './store/index.js'
+
+export default {
+  data() {
+    return {
+      // store,
+      // ...
+    };
+  },
+
+  props: {
+    // ...
+  },
+
+  methods: {
+    // ...
+  },
+
+  components: {},
+
+  created() {
+    // ...
+  },
+};
+</script>
+
+<template></template>
+
+<style lang="scss">
+@use "./assets/scss/global.scss";
+</style>
