c9023de207fe9b160fcbd6c54d8781e5e8dd29ca
jonathanlombardo <jonathanlombardo90@gmail.com>
2024-02-21 13:00:24 +0100

    style and fix: Pricing Plans

--- src/components/sections/PricingPlans.vue
+++ src/components/sections/PricingPlans.vue
@@ -77,6 +77,8 @@ export default {
           },
         ],
       },
+
+      activeIndex: 0,
     };
   },
 
@@ -88,6 +90,14 @@ export default {
     getUrlImg(img) {
       const imgUrl = new URL("../../assets/img/" + img, import.meta.url);
       return imgUrl.href;
+    },
+
+    isCurrentIndex(index) {
+      return this.activeIndex == index;
+    },
+
+    goToIndex(index) {
+      this.activeIndex = index;
     },
   },
 
@@ -101,13 +111,13 @@ export default {
 
 <template>
   <div class="wrapper">
-    <h2>Pricing Plans</h2>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, itaque.</p>
+    <h2 class="text-center">Pricing Plans</h2>
+    <p class="main-p text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, itaque.</p>
     <table>
       <thead>
         <tr>
           <th>Save up to 40% by paying weekly</th>
-          <th v-for="plan in planDetails.plans">
+          <th v-for="(plan, index) in planDetails.plans" :class="isCurrentIndex(index) ? 'active' : ''" @click="goToIndex(index)">
             <img :src="getUrlImg(plan.icon)" :alt="`${plan.name} icon`" />
             <h3>{{ plan.name }}</h3>
             <strong>${{ plan.price }}</strong>
@@ -117,25 +127,132 @@ export default {
       <tbody>
         <tr>
           <td>Number of Courses</td>
-          <td v-for="plan in planDetails.plans">{{ plan.nCourses }}</td>
+          <td v-for="(plan, index) in planDetails.plans" @click="goToIndex(index)">{{ plan.nCourses }}</td>
         </tr>
         <tr>
           <td>Time</td>
-          <td v-for="plan in planDetails.plans">{{ plan.time }}</td>
+          <td v-for="(plan, index) in planDetails.plans" @click="goToIndex(index)">{{ plan.time }}</td>
         </tr>
         <tr v-for="notion in planDetails.notions">
           <td>{{ notion.name }}</td>
-          <td v-for="plan in planDetails.plans">{{ plan[notion.key] }}</td>
+          <td v-for="(plan, index) in planDetails.plans" @click="goToIndex(index)" class="icons">
+            <font-awesome-icon v-if="plan[notion.key] === true" icon="fa-solid fa-check" class="value active" />
+            <span v-else-if="plan[notion.key]" class="value active">{{ plan[notion.key] }}</span>
+            <font-awesome-icon v-else icon="fa-solid fa-xmark" class="value" />
+          </td>
         </tr>
       </tbody>
       <tfoot>
         <tr>
           <td></td>
-          <td v-for="plan in planDetails.plans">GET</td>
+          <td v-for="(plan, index) in planDetails.plans" @click="goToIndex(index)">
+            <app-btn class="btn" text="GET IT NOW" />
+          </td>
         </tr>
       </tfoot>
     </table>
   </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@use "../../assets/scss/partials/mixins" as *;
+@use "../../assets/scss/partials/var" as *;
+.text-center {
+  text-align: center;
+}
+
+.btn {
+  border: none;
+  background-color: $bg-primary;
+  color: $col-light;
+  font-weight: bold;
+}
+
+table {
+  width: 100%;
+  margin-top: $space-xl;
+  border-spacing: 0;
+
+  img {
+    margin: auto;
+    margin-bottom: $space-m;
+  }
+
+  tr {
+    td,
+    th {
+      border-top: 1px solid $border-bright2;
+      border-right: 1px solid $border-bright2;
+
+      padding: $space-m;
+      text-align: center;
+
+      background-color: $bg-light;
+
+      &:first-child {
+        border-left: 1px solid $border-bright2;
+        // ----------------
+        // writing-mode: vertical-lr;
+        // transform: rotate(180deg);
+        // ----------------
+      }
+
+      &:not(:first-child) {
+        cursor: pointer;
+      }
+    }
+
+    th {
+      font-size: 1.6rem;
+      color: $col-dark-bright4;
+      width: calc(100% / 4);
+      // ----------------
+      // width: auto;
+      // ----------------
+
+      border-top: 5px solid $border-primary-light;
+
+      // ----------------
+      // &:first-child {
+      //   border-top: 1px solid $border-bright2;
+      //   border-bottom: 5px solid $border-primary-light;
+      // }
+      // ----------------
+
+      &:not(:first-child) {
+        background-color: $bg-light-info;
+        text-transform: capitalize;
+
+        &.active {
+          border-top: 5px solid $border-primary;
+        }
+      }
+    }
+
+    td {
+      font-size: 1.3rem;
+      color: $col-dark-bright6;
+
+      &.icons {
+        .value {
+          font-size: 1.3em;
+          color: $col-dark-bright8;
+          &.active {
+            color: $col-primary;
+          }
+        }
+      }
+    }
+  }
+  tfoot {
+    td {
+      border-bottom: 1px solid $border-bright2;
+
+      &:first-child {
+        border-left: none;
+        border-bottom: none;
+      }
+    }
+  }
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -6,9 +6,9 @@ import AppBtn from "./components/AppBtn.vue";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { faTwitter, faFacebookF, faInstagram } from "@fortawesome/free-brands-svg-icons";
-import { faPhone, faClock, faBars, faBagShopping, faMagnifyingGlass, faCircle, faCheck, faUser, faTag } from "@fortawesome/free-solid-svg-icons";
+import { faPhone, faClock, faBars, faBagShopping, faMagnifyingGlass, faCircle, faCheck, faUser, faTag, faXmark } from "@fortawesome/free-solid-svg-icons";
 import { faCircleDot } from "@fortawesome/free-regular-svg-icons";
-library.add(faTwitter, faFacebookF, faInstagram, faPhone, faClock, faBars, faBagShopping, faMagnifyingGlass, faCircle, faCircleDot, faCheck, faUser, faTag);
+library.add(faTwitter, faFacebookF, faInstagram, faPhone, faClock, faBars, faBagShopping, faMagnifyingGlass, faCircle, faCircleDot, faCheck, faUser, faTag, faXmark);
 
 const app = createApp(App);
 
